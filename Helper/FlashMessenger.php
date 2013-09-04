a3f612f696e8bb1620f089bfd6477761f8ee6c23
Matthew Weier O'Phinney <matthew@zend.com>
2013-09-04 10:00:25 -0500

    zendframework/zf2@f3ab0b1536276e3f389d81db12723f977a62c29d (1378298919)

--- Helper/FlashMessenger.php
+++ Helper/FlashMessenger.php
@@ -31,7 +31,6 @@ class FlashMessenger extends AbstractTranslatorHelper implements ServiceLocatorA
         PluginFlashMessenger::NAMESPACE_ERROR => 'error',
         PluginFlashMessenger::NAMESPACE_SUCCESS => 'success',
         PluginFlashMessenger::NAMESPACE_DEFAULT => 'default',
-        PluginFlashMessenger::NAMESPACE_WARNING => 'warning',
     );
 
     /**
@@ -107,8 +106,11 @@ class FlashMessenger extends AbstractTranslatorHelper implements ServiceLocatorA
 
         // Prepare classes for opening tag
         if (empty($classes)) {
-            $classes = isset($this->classMessages[$namespace]) ?
-                $this->classMessages[$namespace] : $this->classMessages[PluginFlashMessenger::NAMESPACE_DEFAULT];
+            if (isset($this->classMessages[$namespace])) {
+                $classes = $this->classMessages[$namespace];
+            } else {
+                $classes = $this->classMessages[PluginFlashMessenger::NAMESPACE_DEFAULT];
+            }
             $classes = array($classes);
         }
 
@@ -122,7 +124,8 @@ class FlashMessenger extends AbstractTranslatorHelper implements ServiceLocatorA
         array_walk_recursive($messages, function($item) use (&$messagesToPrint, $escapeHtml, $translator, $translatorTextDomain) {
             if ($translator !== null) {
                 $item = $translator->translate(
-                        $item, $translatorTextDomain
+                    $item,
+                    $translatorTextDomain
                 );
             }
             $messagesToPrint[] = $escapeHtml($item);
